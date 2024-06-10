137179b388c24877919de6c5d3aaca1f248f2ed6
N-Otsubo <otsubo@morijyobi.ac.jp>
2024-06-10 11:30:25 +0900

    init

---
+++ app/sample_app/hello/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from hello import views as hello
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path("", hello.index, name='index'),
+    path("ja/", hello.index_ja),
+    path("login/", hello.login_view, name='login'),
+    path("logout/", hello.logout, name='logout'),
+    path("book_r/", hello.book_register, name='book_r'),
+
+    path("redirect/", hello.index_redirect),
+    path("redirect2/", RedirectView.as_view(url="/hello/ja")),
+    # path("hello/<int:user_id>", hello.index), # hello/1
+]
