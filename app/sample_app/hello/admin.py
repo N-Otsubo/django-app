08979e299d0f1805a145bb03088ebc69656b95fb
N-Otsubo <otsubo@morijyobi.ac.jp>
2024-06-12 00:35:32 +0900

    delete

--- app/sample_app/hello/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, Publisher, Author, Book
-
-
-# Register your models here.
-admin.site.register(CustomUser)
-admin.site.register(Publisher)
-admin.site.register(Author)
-admin.site.register(Book)
